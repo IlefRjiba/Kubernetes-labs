18ef63a3b8190fa5a5db16ab8244416c3c17632f
IlefRjiba <ilef.rjiba@insat.ucar.tn>
2025-01-27 21:13:14 +0100

    script to delete all kubernetes resources

---
+++ deleteall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Deleting all resources in all namespaces..."
+
+  # Delete all resources in the namespace
+  kubectl delete all --all -n irjiba
+
+  # Optionally delete configmaps, secrets, and other resource types
+  kubectl delete configmap --all -n irjiba
+  kubectl delete secret --all -n irjiba
+  kubectl delete pvc --all -n irjiba
+  kubectl delete serviceaccount --all -n irjiba
+  kubectl delete ingress -n irjiba
+
+
+echo "All resources have been deleted."
