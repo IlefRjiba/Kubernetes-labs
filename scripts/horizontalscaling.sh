2403c2df50d953d646471743e48953c0ebe654c1
IlefRjiba <ilef.rjiba@insat.ucar.tn>
2025-01-27 23:50:46 +0100

    challenge 14

--- scripts/horizontalscaling.sh
+++ scripts/horizontalscaling.sh
@@ -1,3 +1,7 @@
-kubectl autoscale deployment webdb --cpu-percent=60 --min=1 --max=10
+kubectl autoscale deployment webdb --cpu-percent=10 --min=1 --max=10
 kubectl autoscale deployment webnodb --cpu-percent=60 --min=1 --max=10
 kubectl autoscale deployment mongodb --cpu-percent=60 --min=1 --max=10
+
+# commande pour tester : 
+# while sleep 0.0001; do wget -q -O- http://webdb.irjiba.net4255.luxbulb.org/ ; done
+
